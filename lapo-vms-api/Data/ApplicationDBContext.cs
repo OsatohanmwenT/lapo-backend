6ed639808ad31643710c8b1bd8a49159eab418ff
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2026-04-21 12:05:39 +0100

    refactor db to save as string for enums

--- lapo-vms-api/Data/ApplicationDBContext.cs
+++ lapo-vms-api/Data/ApplicationDBContext.cs
@@ -10,4 +10,15 @@ public class ApplicationDBContext(DbContextOptions<ApplicationDBContext> options
     public DbSet<VisitItem> VisitItem => Set<VisitItem>();
     public DbSet<VisitorIdentification> VisitorIdentification => Set<VisitorIdentification>();
     public DbSet<WorkerDetails> WorkerDetails => Set<WorkerDetails>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Visit>()
+            .Property(v => v.Status)
+            .HasConversion<string>();
+
+        modelBuilder.Entity<Visitor>()
+            .Property(v => v.VisitorType)
+            .HasConversion<string>();
+    }
 }
