294c927298f75a6ab547ecfd9663c50523b5cd35
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2026-04-27 13:08:59 +0100

    add user endpoints

--- lapo-vms-api/Dtos/Visit/CheckOutVisitDto.cs
+++ lapo-vms-api/Dtos/Visit/CheckOutVisitDto.cs
@@ -5,5 +5,5 @@ namespace lapo_vms_api.Dtos.Visit;
 public class CheckOutVisitDto
 {
     [Range(1, int.MaxValue)]
-    public required string StaffId { get; set; };
+    public required string StaffId { get; set; }
 }
